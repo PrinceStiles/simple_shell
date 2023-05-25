5b6cf8b630070bbba8e7107dd7d44f5060b12320
Henrietta Obioha <Henriettaobioha1@gmail.com>
2023-05-25 02:28:05 -0700

    shell_updates

--- shell.h
+++ shell.h
@@ -91,7 +91,8 @@ typedef struct passinfo
 } info_t;
 
 #define INFO_INIT
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL,
+{
+	(NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL,
 0, 0, 0)
 
 /**
